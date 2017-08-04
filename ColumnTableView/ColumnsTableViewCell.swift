9894b8fe5759c9c3dc465651437762b9e0b87191
Jorge Xavier <mr.jorge.xavier@gmail.com>
2017-08-04 00:33:21 -0300

    adicionado container view via constraints programaticamente na cell de colunas, e adicionado datasource de teste no contorller

--- ColumnTableView/ColumnsTableViewCell.swift
+++ ColumnTableView/ColumnsTableViewCell.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ColumnsTableViewCell: UITableViewCell {
-
+    private var viewsAreSettedUp: Bool = false
+    let containerView: ColumnsViewContainer = ColumnsViewContainer()
+    
+    
+    private func setupViews(){
+        if self.viewsAreSettedUp { return }
+                
+        self.containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(containerView)
+        
+        var constraintsToActivate: [NSLayoutConstraint] = []
+        constraintsToActivate.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "H:|[container]|", metrics: nil, views: ["container":self.containerView]))
+        constraintsToActivate.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "V:|[container]|", metrics: nil, views: ["container":self.containerView]))
+        
+        NSLayoutConstraint.activate(constraintsToActivate)
+        
+        self.viewsAreSettedUp = true
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupViews()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.setupViews()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- ColumnTableView/ViewController.swift
+++ ColumnTableView/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
+    let colors: [UIColor] = [#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1),#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1),#colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1),#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1),#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1),#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1),#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1),#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)]
+    
     @IBOutlet weak var tableView: ColumnsTableView!
     
     override func viewDidLoad() {
@@ -25,11 +27,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PrototypeCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PrototypeCell", for: indexPath) as! ColumnsTableViewCell
+        cell.containerView.backgroundColor = colors[indexPath.row]
         cell.selectionStyle = .none
         return cell
     }
