cb8fdb589bf424999cbe060e49789796f5a5aadd
Jorge Xavier <mr.jorge.xavier@gmail.com>
2017-08-03 22:23:18 -0300

    Initial Commit

---
+++ ColumnTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColumnTableView
+//
+//  Created by Jorge Luis on 03/08/17.
+//  Copyright © 2017 Jorge Luis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
